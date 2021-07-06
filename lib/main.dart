0ae0f4ac0807ee17604f5f3fe986ec39cc695e0e
jisung <sdh210421@sdh.hs.kr>
2021-07-06 20:11:43 +0900

    slbal

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,46 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('주지성',style: TextStyle(fontSize: 30),),
-            Text('App Developer',style: TextStyle(fontSize: 30),),
-            Text('010-2554-1615',style: TextStyle(fontSize: 30),),
-            Text('sdh210421@sdh.hs.kr',style: TextStyle(fontSize: 30),),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: 50),
+                width: 200,
+                height: 200,
+                child: Image.network(
+                    'https://pbs.twimg.com/profile_images/784043167377002496/qT2jUBkx_400x400.jpg'),
+              ),
+              Text(
+                '주지성',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                'App Developer',
+                style: TextStyle(fontSize: 30),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                '010-2554-1615',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                'sdh210421@sdh.hs.kr',
+                style: TextStyle(fontSize: 30),
+              ),
+              ElevatedButton(onPressed: () {}, child: Text('슬라이드 페이지 이동'))
             ],
-        ), 
+          ),
+        ),
       ),
     );
   }
